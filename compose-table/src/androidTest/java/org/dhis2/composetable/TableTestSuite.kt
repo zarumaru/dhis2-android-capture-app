718922741fb9eb9f3d7a9d142980ea103dbdc875
JaimeToca <jaime.toca.munoz@gmail.com>
2022-08-24 14:45:58 +0200

    fix: add new suite

---
+++ compose-table/src/androidTest/java/org/dhis2/composetable/TableTestSuite.kt
@@ -0,0 +1,11 @@
+package org.dhis2.composetable
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    ColumnTableTest::class,
+    RowTableTest::class
+)
+class TableTestSuite
